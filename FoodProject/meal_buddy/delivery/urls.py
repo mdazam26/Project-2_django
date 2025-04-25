254cdbb30fc642454f0e6f16cc7b2d66a9320f8a
mdazam26 <azam456alam@gmail.com>
2025-04-25 18:39:56 +0530

    save

--- FoodProject/meal_buddy/delivery/urls.py
+++ FoodProject/meal_buddy/delivery/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
+    
     path('open_signin', views.open_signin, name='open_signin'),
+    path('signin', views.signin, name='signin'),
+
     path('open_signup', views.open_signup, name='open_signup'),
     path('signup', views.signup, name='signup'),
 
-    path('signin', views.signin, name='signin'),
+    
 ]
